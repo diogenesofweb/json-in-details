7d9b89fceaed3335f9d63d280bc88ef217d891f4
Diogenes <diogenesofweb@gmail.com>
2023-07-31 18:16:22 +0300

    Up: 2023-07-31 18:16:22

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,28 +33,30 @@
 
 	<section class="tips f-mono">
 		<Dtl heading="Search" open>
-			<ol>
+			<ul>
 				<li>
 					Press <i>COLLAPSE </i>icon
-					<Icon name="unfold_less_double" />
+					<Icon name="unfold_less_double" />.
 				</li>
 
-				<li>Press <i>Ctrl+F</i></li>
-				<li>Browser will auto expand matched nodes</li>
-			</ol>
+				<li>Press <i>Ctrl+F</i>.</li>
+				<li>Browser will auto expand matched nodes.</li>
+			</ul>
 		</Dtl>
 
 		<Dtl heading="Keymaps (Vim inspired)">
-			<p><b>h</b> | ArrowLeft - move focus to the parent node</p>
-			<p><b>j</b> | ArrowDown - move focus to the next sibling node</p>
-			<p><b>k</b> | ArrowUp - move focus to the previous sibling node</p>
-			<p><b>l</b> | ArrowRight - move focus to the child node</p>
+			<ul>
+				<li><b>h</b> | <b>ArrowLeft</b> - Move focus to the parent node.</li>
+				<li><b>j</b> | <b>ArrowDown</b> - Move focus to the next sibling node.</li>
+				<li><b>k</b> | <b>ArrowUp</b> - Move focus to the previous sibling node.</li>
+				<li><b>l</b> | <b>ArrowRight</b> - Move focus to the child node.</li>
 
-			<p><b>c</b> - fold all child nodes</p>
-			<p><b>e</b> - unfold all child nodes</p>
+				<li><b>c</b> - Fold all child nodes.</li>
+				<li><b>e</b> - Unfold all child nodes.</li>
 
-			<p><b>y</b> - copy node content</p>
-			<p><b>p</b> - copy node path</p>
+				<li><b>y</b> - Copy node content.</li>
+				<li><b>p</b> - Copy node path.</li>
+			</ul>
 		</Dtl>
 
 		<Dtl heading="Convert JSON">
@@ -78,6 +80,36 @@
 				structs is done with
 				<a href="https://github.com/mholt/json-to-go">json-to-go</a>.
 			</p>
+		</Dtl>
+
+		<Dtl heading="App description">
+			<p>Elegant UI:</p>
+			<ul>
+				<li>Foldable and filterable and focusable nodes.</li>
+				<li>Show node path on focus and hover.</li>
+				<li>Configurable clickable links.</li>
+				<li>Light and dark theme.</li>
+			</ul>
+
+			<p>Functionality:</p>
+
+			<ul>
+				<li>Filter nodes.</li>
+				<li>Collapse all nodes.</li>
+				<li>Expand all nodes.</li>
+				<li>Save JSON.</li>
+				<li>Copy JSON.</li>
+				<li>Copy node.</li>
+				<li>Copy node path.</li>
+				<li>Switch between formatted and raw data.</li>
+
+				<li>Sticky panels.</li>
+				<li>Converting JSON.</li>
+				<li>(Shift-)Tab navigation.</li>
+				<li>Keymaps (Vim inspired).</li>
+				<li>History.</li>
+				<li>New JSON entry via file upload, fetch URL or insert JSON string.</li>
+			</ul>
 		</Dtl>
 
 		<Dtl heading="What is JSON ?">
@@ -107,7 +139,7 @@
 		padding-block: 1em;
 		/* background: var(--bg0); */
 	}
-	p > b {
+	li > b {
 		color: var(--fg-gamma);
 	}
 	p > a {
@@ -137,5 +169,10 @@
 		margin-block: 3em;
 		display: grid;
 		gap: 1em;
+	}
+	li {
+		padding-block: 0.5ch;
+		list-style-position: inside;
+		list-style-type: disc;
 	}
 </style>
