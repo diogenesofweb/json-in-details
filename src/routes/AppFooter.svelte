6659da15f7c235a0815bf74d9a67783ee69caa47
Diogenes <diogenesofweb@gmail.com>
2023-08-11 13:00:46 +0300

    Up: 2023-08-11 13:00:46

--- src/routes/AppFooter.svelte
+++ src/routes/AppFooter.svelte
@@ -1,38 +1,54 @@
 <script>
-	import AppHomeLink from './AppHomeLink.svelte';
+	// import AppHomeLink from './AppHomeLink.svelte';
 	// console.log('x')
 </script>
 
 <footer class="alpha">
-	<section class="fsb g1">
-		<a
-			href="https://chrome.google.com/webstore/detail/functional-json-viewer/cokkmplppnaigoodkkjopoblfaelibab"
-			title="Install extension"
-		>
-			<img src="/cws.png" alt="Available in the Chrome Web Store" width="206" height="58" />
-		</a>
-
-		<div class="more">
-			<div class="hh">More:</div>
-			<div style="display: grid;">
-				<a href="https://hotatap.delphic.top/app" class="btn text alpha">
-					<span>Web client for REST API testing</span>
+	<section class="">
+		<ul>
+			<li>
+				<a
+					href="https://chrome.google.com/webstore/detail/functional-json-viewer/cokkmplppnaigoodkkjopoblfaelibab"
+					title="Install extension"
+				>
+					<img src="/cws.png" alt="Available in the Chrome Web Store" width="206" height="58" />
 				</a>
-			</div>
-		</div>
+			</li>
+		</ul>
 	</section>
 
-	<nav class="fsb g1">
-		<div>
-			<AppHomeLink />
-		</div>
+	<section>
+		<p>Info:</p>
 
-		<div class="fsb">
-			<!-- svelte-ignore missing-declaration -->
-			<a href={PKG['homepage']} class="btn text"> Github repo </a>
-			<a href="https://www.npmjs.com/package/json-in-details" class="btn text"> NPM package </a>
-		</div>
-	</nav>
+		<ul>
+			<li>
+				<!-- svelte-ignore missing-declaration -->
+				<a href={PKG['homepage']} class="btn text"> Github repository </a>
+			</li>
+
+			<li>
+				<a href="https://www.npmjs.com/package/json-in-details" class="btn text"> NPM package </a>
+			</li>
+		</ul>
+	</section>
+
+	<section>
+		<p>Related projects:</p>
+
+		<ul>
+			<li>
+				<a href="https://hotatap.delphic.top/app" class="btn text">
+					Web client for REST API testing
+				</a>
+			</li>
+
+			<li>
+				<a href="https://app.tabulamax.com" class="btn text">
+					Design DB tables, generate SQL and KnexJS migraions, TS interfaces, GO structs.
+				</a>
+			</li>
+		</ul>
+	</section>
 </footer>
 
 <style>
@@ -44,31 +60,10 @@
 	}
 
 	section {
-		display: flex;
-		margin-bottom: 3em;
-		gap: 2em;
+		padding-block: 1rem;
 	}
 
-	:where(section, nav) > :first-child {
-		flex-grow: 100;
-	}
-	.fsb,
-	.more {
-		flex-grow: 1;
-		justify-self: end;
-		/* background: gray; */
-	}
-
-	.hh {
-		text-align: center;
-	}
-
-	/* nav { */
+	/* .links { */
 	/* 	background: gray; */
 	/* } */
-
-	nav a {
-		/* background: gray; */
-		flex-grow: 1;
-	}
 </style>
